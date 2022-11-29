891a72fead4db40dd1ec31fd945e69ac7d6bf062
Ezoka27 <ezokagfx@gmail.com>
2022-11-29 14:10:20 +0100

    Modification du style pour la recherche

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Vaccination COVID';
+  title = '💉💉Vaccination COVID💉💉';
 }
