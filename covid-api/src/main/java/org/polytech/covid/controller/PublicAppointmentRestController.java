ddccf47ab287ada21ba041f87d18f2ea7be42202
Ezoka27 <ezokagfx@gmail.com>
2022-12-28 22:13:59 +0100

    Fix de la prise de rdv sur le back

--- covid-api/src/main/java/org/polytech/covid/controller/PublicAppointmentRestController.java
+++ covid-api/src/main/java/org/polytech/covid/controller/PublicAppointmentRestController.java
@@ -22,12 +22,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublicAppointmentRestController {
     @Autowired
     private AppointmentRepository appointmentRepository;
+    @Autowired
     private PatientRepository patientRepository;
+    @Autowired
     private VaccinationCenterRepository vaccinationCenterRepository;
 
     @GetMapping(path = "api/public/appointments")
     public List<Appointment> getAllAppointments(){
         return appointmentRepository.findAll();
+    }
+    @GetMapping(path = "api/public/patients")
+    public List<Patient> getAllPatients(){
+        return patientRepository.findAllPatients();
+    }
+
+    @GetMapping(path = "api/public/patient")
+    public Patient getPatient(
+            @RequestParam("id_patient") Integer ID)
+    {
+        return patientRepository.findById(ID).get();
     }
 
     @PostMapping(path = "api/public/booking")
@@ -45,6 +58,33 @@ public class PublicAppointmentRestController {
                 return new ResponseEntity<Appointment>(appointment, HttpStatus.CREATED);
             }
 
+    // @GetMapping(path = "api/public/test")
+    // public String Ajout(@RequestParam Map<String, String> requestParams) {
+    //             System.out.println("GROS ZEUB SA MERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    //             Integer id_patient = Integer.parseInt(requestParams.get("id_patient"));
+    //             Integer id_vaccination_center= Integer.parseInt(requestParams.get("id_vaccination_center"));
+    //             String date= requestParams.get("date");
+    //             return "Parameters are : " + "id patient : " + Integer.toString(id_patient) + " id center : " + Integer.toString(id_vaccination_center) + " date : " + date;
+    //         }
+    @GetMapping(path = "api/public/test")
+    public ResponseEntity<Appointment> Ajout(@RequestParam Map<String, String> requestParams) {
+        Integer id_patient = Integer.parseInt(requestParams.get("id_patient"));
+        System.out.println("ID PATIENT : " + Integer.toString(id_patient));
+        Integer id_vaccination_center= Integer.parseInt(requestParams.get("id_vaccination_center"));
+        System.out.println("ID CENTRE : " + Integer.toString(id_vaccination_center));
+        String date= requestParams.get("date");
+        System.out.println("DATE : " + date);
+        Integer id_appointment = Integer.parseInt(requestParams.get("id_appointment"));
+        System.out.println("ID RDV : " + Integer.toString(id_appointment));
+        Patient patient = new Patient();
+        patient = patientRepository.findById(id_patient).get();
+        VaccinationCenter vaccination_center = new VaccinationCenter();
+        vaccination_center = vaccinationCenterRepository.findById(id_vaccination_center).get();
+        Appointment appointment = new Appointment(id_appointment,patient,vaccination_center,date);
+        appointmentRepository.save(appointment);
+        return new ResponseEntity<Appointment>(appointment, HttpStatus.CREATED);
+            }
+    
     // @PostMapping(path = "api/public/booking")
     // public void postAppointment(        
     //         @RequestParam(name="id_appointment") Integer id_appointment,
--- covid-api/src/main/java/org/polytech/covid/repository/PatientRepository.java
+++ covid-api/src/main/java/org/polytech/covid/repository/PatientRepository.java
@@ -1,12 +1,17 @@
 package org.polytech.covid.repository;
 
 import java.util.Optional;
+import java.util.List;
 
 import org.polytech.covid.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient,Integer>{
     public Optional<Patient> findById(Integer id);
+
+    @Query("from Patient")
+    public List<Patient> findAllPatients();
 }
